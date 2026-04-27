81c927dd33ffd228993aea8b4e050f1319726396
Naveen Negi <ext.naveen.negi@apcoa.eu>
2026-04-27 15:03:22 +0200

    fix: point formula URL to homebrew-tap release (source repo is private)

--- Formula/underscore.rb
+++ Formula/underscore.rb
@@ -1,7 +1,7 @@
 class Underscore < Formula
   desc "Spatial visualization for C# and Java codebases"
   homepage "https://github.com/logPhase/underscore-cli"
-  url "https://github.com/logPhase/underscore-cli/releases/download/v0.1.5-native/underscore-0.1.5-macos-arm64.tar.gz"
+  url "https://github.com/logPhase/homebrew-tap/releases/download/v0.1.5/underscore-0.1.5-macos-arm64.tar.gz"
   sha256 "6de50ef6d15d76cfd49b2757f7ac8373b94500048a52d6c13fd867ca6dd940bc"
   version "0.1.5"
   license :cannot_represent
